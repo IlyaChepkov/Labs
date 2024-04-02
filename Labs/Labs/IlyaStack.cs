b4d3728b0c2514e6817d71a52735851ba6855700
IlyaChepkov <ilya.chepkov2011@gmail.com>
2024-04-02 18:10:37 +0300

    Stack

---
+++ Labs/Labs/IlyaStack.cs
@@ -0,0 +1,39 @@
+﻿namespace Labs
+{
+    internal class IlyaStack<T>
+    {
+        OneWayNode<T> top;
+        int lenght;
+
+        public int Lenght
+        {
+            get { return lenght; }
+        }
+
+        public void Push(T data)
+        {
+            OneWayNode<T> newTop = new OneWayNode<T> 
+            {
+                next = top,
+                data = data 
+            };
+            top = newTop;
+            lenght++;
+        }
+
+        public T Pop()
+        {
+            if (top == null) new Exception("стек пустой");
+            T value = top.data;
+            top = top.next;
+            lenght--;
+            return value;
+        }
+
+        class OneWayNode<T>
+        {
+            public OneWayNode<T> next;
+            public T data;
+        }
+    }
+}
--- Labs/Labs/Program.cs
+++ Labs/Labs/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Labs;
+
+public class Program
+{
+    public static void Main()
+    {
+        bool mode = bool.Parse(Console.ReadLine());
+        if(mode)
+        {
+            UserMode();
+        }
+        else
+        {
+            IlyaStack<int> stack = new IlyaStack<int>();
+            while (true)
+            {
+                int value = int.Parse(Console.ReadLine());
+                if (value == 0) break;
+                if (value < 0) continue;
+                stack.Push(value);
+            }
+            while (stack.Lenght > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+
+    public static void UserMode()
+    {
+        IlyaStack<int> stack = new IlyaStack<int>();
+        while (true)
+        {
+            Console.WriteLine("Введите число");
+            int value = int.Parse(Console.ReadLine());
+            if (value == 0) break;
+            if (value < 0) continue;
+            stack.Push(value);
+        }
+        while (stack.Lenght > 0)
+        {
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
