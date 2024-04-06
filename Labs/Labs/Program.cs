72853d24bf66bf1a667425871f6afa725e85e96c
IlyaChepkov <ilya.chepkov2011@gmail.com>
2024-04-06 21:09:03 +0300

    исправление ошибок 1

--- Labs/Labs/Program.cs
+++ Labs/Labs/Program.cs
@@ -3,34 +3,18 @@ using Labs;
 
 public class Program
 {
-    public static void Main()
+    public static void Main(string[] mode)
     {
-        bool mode = bool.Parse(Console.ReadLine());
-        if(mode)
+        if (mode.Length == 0 || mode[0] == "true")
+            StackUserMode();
+        else if (mode[0] == "false")
         {
-            UserMode();
-        }
-        else
-        {
-            IlyaStack<int> stack = new IlyaStack<int>();
-            while (true)
-            {
-                int value = int.Parse(Console.ReadLine());
-                if (value == 0) break;
-                
-                stack.Push(value);
-            }
-            while (stack.Lenght > 0)
-            {
-                int value = stack.Pop();
-                if (value < 0) continue;
-                Console.WriteLine(value);
-            }
+            StackProgramMode();
         }
         Console.ReadLine();
     }
 
-    public static void UserMode()
+    public static void StackUserMode()
     {
         IlyaStack<int> stack = new IlyaStack<int>();
         Console.WriteLine("Введите поледовательность чисел (конец окончания последовательности 0)");
@@ -38,6 +22,24 @@ public class Program
         {
             int value = int.Parse(Console.ReadLine());
             if (value == 0) break;
+            stack.Push(value);
+        }
+        while (stack.Lenght > 0)
+        {
+            int value = stack.Pop();
+            if (value < 0) continue;
+            Console.WriteLine(value);
+        }
+    }
+
+    public static void StackProgramMode()
+    {
+        IlyaStack<int> stack = new IlyaStack<int>();
+        while (true)
+        {
+            int value = int.Parse(Console.ReadLine());
+            if (value == 0) break;
+
             stack.Push(value);
         }
         while (stack.Lenght > 0)
